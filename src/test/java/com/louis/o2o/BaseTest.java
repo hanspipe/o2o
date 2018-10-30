738ed81659c92f72580d75ddeef528d818a45ff3
hanspipe <464897127@qq.com>
2018-10-30 22:34:56 +0800

    logback

---
+++ src/test/java/com/louis/o2o/BaseTest.java
@@ -0,0 +1,18 @@
+package com.louis.o2o;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * @author louis
+ * 2018年10月30日
+ * <p>Description: 配置spring和Junit整合，junit启动时加载springIOC容器</p>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件的位置
+@ContextConfiguration({"classpath:spring/spring-dao.xml","classpath:spring/spring-service.xml"})
+public class BaseTest {
+	
+}
